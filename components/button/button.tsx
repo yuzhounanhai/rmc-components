ced407575f936a6fcee1b182b1dc7773642f1a51
yuzhounanhai <yuzhounanhai@vip.qq.com>
2021-01-11 17:39:05 +0800

    引入修改 import * as React -> import React

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import cn from 'classnames';
 import LoadingOutlined from '@ant-design/icons/LoadingOutlined';
 import { defaultPrefixCls } from '@/_config/dict';
