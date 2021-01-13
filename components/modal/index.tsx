ee4763d534290459eef9535f3f8a3b070557c4b2
yuzhounanhai <yuzhounanhai@vip.qq.com>
2021-01-13 12:23:34 +0800

    增加Modal组件

---
+++ components/modal/index.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PortalFactory, {
+  contentRenderFn,
+} from '@/portalFactory/index';
+import BaseModal, {
+  QuickModalProps,
+} from './modal';
+
+export type ModalType = typeof BaseModal & {
+  info: (config?: QuickModalProps) => void,
+  confirm: (config?: QuickModalProps) => void,
+  showCustom: (config?: QuickModalProps) => void,
+};
+
+const Modal = BaseModal as ModalType;
+
+// Feature 是否存在更好的实现方式，如Symbol
+const closeFnCallFlag = '@@__symbols__fromCloseFn_';
+
+const renderModal: contentRenderFn = (props: QuickModalProps, update, destory) => {
+  const {
+    content,
+    children,
+    onOk,
+    onHide,
+    onCancel,
+    ...restProps
+  } = props;
+  const isInClosing = !!props[closeFnCallFlag];
+  delete restProps[closeFnCallFlag];
+  const close = () => {
+    if (isInClosing) {
+      return;
+    }
+    const newProps = {
+      ...props,
+      [closeFnCallFlag]: true,
+      visible: false,
+    };
+    update(newProps);
+  };
+  const onHideFn = () => {
+    typeof onHide === 'function' && onHide();
+    // 判断是否由close方法引发的onHide
+    if (isInClosing) {
+      destory();
+    }
+  };
+  const onOkFn = () => {
+    typeof onOk === 'function'
+        ? onOk(close)
+        : close();
+  }
+  const onCancelFn = () => {
+    typeof onCancel === 'function'
+        ? onCancel(close)
+        : close();
+  }
+  return (
+    <BaseModal
+      {...restProps}
+      onHide={onHideFn}
+      onOk={onOkFn}
+      onCancel={onCancelFn}
+
+    >
+      {content || children}
+    </BaseModal>
+  );
+};
+
+Modal.info = function(config) {
+  const modalProps = {
+    confirmable: false,
+    cancelText: '确定',
+    ...(config || {})
+  };
+  const pf = PortalFactory(renderModal);
+  pf.render(modalProps);
+};
+
+Modal.confirm = function(config) {
+  const modalProps = {
+    ...(config || {})
+  };
+  const pf = PortalFactory(renderModal);
+  pf.render(modalProps);
+}
+
+Modal.showCustom = function(config) {
+  const modalProps = {
+    maskClosable: true,
+    customStructure: true,
+    ...(config || {})
+  };
+  const pf = PortalFactory(renderModal);
+  pf.render(modalProps);
+}
+
+export default Modal;
---
+++ components/modal/modal.tsx
@@ -0,0 +1,176 @@
+import React from 'react';
+import cn from 'classnames';
+import CloseOutlined from '@ant-design/icons/CloseOutlined';
+import FadeIn from '@/fade/fadeIn';
+import { defaultPrefixCls } from '@/_config/dict';
+
+export interface ModalCommonProps {
+  className?: string;
+  visible?: boolean;
+  width?: string,
+  mask?: boolean;
+  maskClosable?: boolean;
+  confirmable?: boolean;
+  customStructure?: boolean;
+  confirmCareful?: boolean;
+  title?: React.ReactNode;
+  cancelText?: React.ReactNode;
+  closeCorner?: boolean;
+  footer?: React.ReactNode;
+  okText?: React.ReactNode;
+  onShow?: () => void;
+  onHide?: () => void;
+}
+
+export interface ModalProps extends ModalCommonProps {
+  onCancel?: (e: React.MouseEvent) => void;
+  onOk?: () => void;
+  children?: React.ReactNode;
+  [key: string]: any;
+};
+
+export interface QuickModalProps extends ModalCommonProps {
+  onCancel?: (close: () => void) => void;
+  onOk?: (close: () => void) => void;
+  content?: React.ReactNode;
+  [key: string]: any;
+};
+
+function Modal(props: ModalProps) {
+  const {
+    className,
+    width,
+    visible = true,
+    mask = true,
+    maskClosable = false,
+    confirmCareful = false,
+    closeCorner = false,
+    confirmable = true,
+    okText = '确定',
+    cancelText = '取消',
+    title,
+    footer,
+    customStructure,
+    children,
+    prefixCls = defaultPrefixCls,
+    onOk,
+    onCancel,
+    onShow,
+    onHide,
+    ...restProps
+  } = props;
+
+  const onClickMask = (e: React.MouseEvent) => {
+    if (maskClosable) {
+      typeof onCancel === 'function' && onCancel(e);
+    }
+  };
+
+  const renderFooter = () => {
+    if (footer === null) {
+      return null;
+    }
+    const okBtn = (
+      <div
+        className={`${prefixCls}-modal-button ${prefixCls}-modal-ok`}
+        onClick={onOk}
+      >
+        {okText}
+      </div>
+    );
+    const cancelBtn = (
+      <div
+        className={`${prefixCls}-modal-button ${prefixCls}-modal-cancel`}
+        onClick={onCancel}
+      >
+        {cancelText}
+      </div>
+    )
+    return (
+      <div className={`${prefixCls}-modal-footer`}>
+        {
+          footer || (
+            confirmCareful ? (
+              <>
+                {Boolean(confirmable) && okBtn}
+                {cancelBtn}
+              </>
+            ) : (
+              <>
+                {cancelBtn}
+                {Boolean(confirmable) && okBtn}
+              </>
+            )
+          )
+        }
+      </div>
+    )
+  };
+
+  const renderedCloseCorner = () => {
+    if (closeCorner) {
+      return (
+        <div className={`${prefixCls}-modal-corner-close`} onClick={onCancel}>
+          <CloseOutlined />
+        </div>
+      )
+    }
+    return null;
+  }
+
+  return (
+    <FadeIn
+      show={visible}
+      needDestroy
+      limitTstProperty
+      onShow={onShow}
+      onHide={onHide}
+    >
+      <div
+        className={cn(`${prefixCls}-modal`, className)}
+        {...restProps}
+      >
+        {
+          !!mask && (
+            <div
+              className={`${prefixCls}-modal-mask`}
+              onClick={onClickMask}
+            />
+          )
+        }
+        {
+          customStructure ? (
+            <div className={`${prefixCls}-modal-custom-content`}>
+              {typeof children === 'function' ? children(onOk, onCancel) : children}
+              {renderedCloseCorner()}
+            </div>
+          ) : (
+            <div
+              className={cn(`${prefixCls}-modal-content`, {
+                [`${prefixCls}-modal-content-no-title`]: !title
+              })}
+              style={width ? {
+                width,
+              } : undefined}
+            >
+              {
+                !!title && (
+                  <div className={`${prefixCls}-modal-header`}>
+                    {title}
+                  </div>
+                )
+              }
+              <div className={`${prefixCls}-modal-body`}>
+                {children}
+              </div>
+              {renderFooter()}
+              {renderedCloseCorner()}
+            </div>
+          )
+        }
+      </div>
+    </FadeIn>
+  )
+}
+
+export default Modal;
