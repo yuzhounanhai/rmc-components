51a08cf4cf267f5a67518b37d1376378ca91991c
yuzhounanhai <yuzhounanhai@vip.qq.com>
2021-01-15 18:18:54 +0800

    useRefState 类型补充

--- components/_hook/useRefState/index.ts
+++ components/_hook/useRefState/index.ts
@@ -1,15 +1,20 @@
 import { useCallback, useRef, useState } from 'react';
 
-export default (defaultValue: any) => {
-  const [, setValue] = useState(defaultValue || false);
-  const valueRef = useRef(defaultValue || false);
-  const realSetValue = useCallback((valueOrFunc: any) => {
-    if (typeof valueOrFunc === 'function') {
-      valueRef.current = valueOrFunc(valueRef.current);
-    } else {
-      valueRef.current = valueOrFunc;
-    }
-    setValue(valueOrFunc);
-  }, []);
+export default <T>(defaultValue: T): [React.MutableRefObject<T>, (value: T) => void | ((cb: (v: T) => T) => void)] => {
+  const [, setValue] = useState<T>(defaultValue);
+  const valueRef = useRef<T>(defaultValue);
+  const realSetValue = useCallback(
+    (
+      valueOrFunc: ((v: T) => T) | T
+    ) => {
+      if (typeof valueOrFunc === 'function') {
+        valueRef.current = (valueOrFunc as ((v: T) => T))(valueRef.current);
+      } else {
+        valueRef.current = valueOrFunc;
+      }
+      setValue(valueOrFunc);
+    },
+    []
+  );
   return [valueRef, realSetValue];
 };
