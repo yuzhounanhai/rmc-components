bfbeb3be3396e44d3c6e92f606054f23ca430ced
yuzhounanhai <yuzhounanhai@vip.qq.com>
2021-03-12 15:02:20 +0800

    入口文件调整

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function pascalCase(name) {
   return name.charAt(0).toUpperCase() + name.slice(1).replace(/-(\w)/g, (m, n) => n.toUpperCase());
 }
 
-// Just import style for https://github.com/ant-design/ant-design/issues/3745
 const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
 
 req.keys().forEach(mod => {
@@ -12,12 +11,7 @@ req.keys().forEach(mod => {
   }
   const match = mod.match(/^\.\/([^_][\w-]+)\/index\.tsx?$/);
   if (match && match[1]) {
-    if (match[1] === 'message' || match[1] === 'notification') {
-      // message & notification should not be capitalized
-      exports[match[1]] = v;
-    } else {
-      exports[pascalCase(match[1])] = v;
-    }
+    exports[pascalCase(match[1])] = v;
   }
 });
 
